c6e25e689e5f7c42285e8a8a53f19f025269fe8f
root <root@testpy.local.tinic.org>
2021-01-31 04:17:57 +0000

    Dependency script.

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+# ubuntu deps
+apt-get update
+apt-get upgrade
+
+apt-get install git gcc python3-dev python3-venv python3-pip gpg
+
+# mongo
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt-get update
+apt-get install -y mongodb-org
+
+# start server
+systemctl enable mongod
+systemctl start mongod
+
+# app-server
+pip3 install numpy
+pip3 install bitstream bitstring pymongo httpserver requests
+
